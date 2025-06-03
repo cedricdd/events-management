47a8ac29ed9d6498e0b6b22839aa1c49f934c7f7
Cedric Den Drijver <c-d-d@hotmail.fr>
2025-06-03 19:45:12 +0200

    add the loaded relation in the paginators links

--- app/LoadRelationships.php
+++ app/LoadRelationships.php
@@ -11,26 +11,38 @@ trait LoadRelationships
     protected function loadRelationships(Model|Collection|LengthAwarePaginator $input, array $allowed = [])
     {
         // Nothing can be loaded
-        if(count($allowed) === 0) return $input;
+        if (count($allowed) === 0)
+            return $input;
 
+        $loadedRelationships = [];
         $relationships = request()->query('with', '');
 
         foreach (explode(',', $relationships) as $relationship) {
             $relationship = strtolower(trim($relationship));
 
             // Skip empty relationships
-            if (empty($relationship)) continue;
+            if (empty($relationship))
+                continue;
 
             // Skip relationships that are not formatted correctly
-            if (preg_match('/[^a-z_.]/', $relationship)) continue;
+            if (preg_match('/[^a-z_.]/', $relationship))
+                continue;
 
             // Skip relationships that are not in the allowed list
-            if (!in_array($relationship, $allowed)) continue;
+            if (!in_array($relationship, $allowed))
+                continue;
 
             // Skip relationships that are already loaded
-            if ($input instanceof Model && $input->relationLoaded($relationship)) continue;
-            
+            if ($input instanceof Model && $input->relationLoaded($relationship))
+                continue;
+
             $input->load($relationship);
+
+            $loadedRelationships[] = $relationship;
+        }
+
+        if ($input instanceof LengthAwarePaginator) {
+            $input->appends(['with' => implode(',', $loadedRelationships)]);
         }
 
         return $input;
